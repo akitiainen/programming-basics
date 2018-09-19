a3429da854d6752a6a5d9b2cf3f7fda8bfd7c2a5
akitiainen <aki.tiainen@hotmail.fi>
2018-09-19 09:06:04 +0300

    condition-tasks initialized

---
+++ condition-tasks/ifTask1/ifTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ifTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program determines if a number is positive, negative or a zero");
+            Console.WriteLine("Input a number");
+            Console.
+        }
+    }
+}
