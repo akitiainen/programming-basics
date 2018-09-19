949053bcbaf1745ad20202e6077f333851a10610
akitiainen <aki.tiainen@hotmail.fi>
2018-09-19 11:24:00 +0300

    ifTask1/Program.cs done

--- condition-tasks/ifTask1/ifTask1/Program.cs
+++ condition-tasks/ifTask1/ifTask1/Program.cs
@@ -7,8 +7,16 @@ namespace ifTask1
         static void Main(string[] args)
         {
             Console.WriteLine("This program determines if a number is positive, negative or a zero");
-            Console.WriteLine("Input a number");
-            Console.
+            Console.Write("Input a number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"Your number {number} is zero");
+            else if (number<0)
+                Console.WriteLine($"Your number {number} is negative");
+            else
+                Console.WriteLine($"Your number {number} is positive");
         }
     }
 }
