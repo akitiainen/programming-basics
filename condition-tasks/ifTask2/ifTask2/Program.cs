650f747a6e8c9516e222c424f30ed379209227f9
akitiainen <aki.tiainen@hotmail.fi>
2018-09-20 09:01:54 +0300

    if task2 v.1

--- condition-tasks/ifTask2/ifTask2/Program.cs
+++ condition-tasks/ifTask2/ifTask2/Program.cs
@@ -6,7 +6,15 @@ namespace ifTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This program sorts out if the number is even or odd");
+            Console.Write("Input a number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number % 2 == 0)
+                Console.WriteLine($"Number {number} is even");
+            else
+                Console.WriteLine($"Number {number} is odd");
         }
     }
 }
