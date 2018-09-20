9128f736d8d0f8d4d0696e72fc741a98ddeb9320
akitiainen <aki.tiainen@hotmail.fi>
2018-09-20 09:43:40 +0300

    ifTask3/Program.cs v0.1

--- condition-tasks/ifTask3/ifTask3/Program.cs
+++ condition-tasks/ifTask3/ifTask3/Program.cs
@@ -6,7 +6,21 @@ namespace ifTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This program defines if a number if positive or negative and if a number is even or odd");
+            Console.Write("Input a number: ");
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isNumber = int.TryParse(userInput, out number);
+
+            if (isNumber)
+            {
+                if (number == 0)
+                    Console.WriteLine("Your number is zero!");
+                else if (number < 0)
+                    Console.WriteLine($"Your number {number} is negative!");
+                else
+                    Console.WriteLine($"Your number {number} is positive!");
+            }
         }
     }
 }
