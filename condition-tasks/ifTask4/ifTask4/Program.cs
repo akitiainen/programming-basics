2296730c7f3040dfd2318e6c78802360303d81fb
akitiainen <aki.tiainen@hotmail.fi>
2018-09-21 08:36:29 +0300

    ifTask4.csproj initialized

---
+++ condition-tasks/ifTask4/ifTask4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ifTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program sorts three numbers from smallest to largest");
+            Console.Write("Input three digits: ");
+            
+        }
+    }
+}
