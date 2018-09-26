80c2e08af86e10a46c7efd80f64764d3f96933db
akitiainen <aki.tiainen@hotmail.fi>
2018-09-26 13:45:12 +0300

    IfTask LipunHinta.csproj initialized

---
+++ IfTask LipunHinta/IfTask LipunHinta/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfTask_LipunHinta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma laskee maatalousnäyttelyn lipun hinnan");
+            Console.WriteLine("Oletko varusmies k/e: ");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "k")
+                Console.WriteLine("Alennuksesi on 50%");
+            else
+            {
+                Console.WriteLine("Oletko opiskelija k/e: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == "k")
+                    Console.WriteLine("Oletko mtk:n jäsen k/e: ");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+}
