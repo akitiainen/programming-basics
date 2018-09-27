83f74108b9dcf87558770da37d5d9bcf1c6cdd03
akitiainen <aki.tiainen@hotmail.fi>
2018-09-27 13:51:21 +0300

    IfTask LipunHinta/Program.cs updated

--- IfTask LipunHinta/IfTask LipunHinta/Program.cs
+++ IfTask LipunHinta/IfTask LipunHinta/Program.cs
@@ -7,20 +7,34 @@ namespace IfTask_LipunHinta
         static void Main(string[] args)
         {
             Console.WriteLine("Tämä ohjelma laskee maatalousnäyttelyn lipun hinnan");
+            int originalPrice = 16;
+            double discount = 0;
             Console.WriteLine("Oletko varusmies k/e: ");
             string userInput = Console.ReadLine();
 
             if (userInput == "k")
-                Console.WriteLine("Alennuksesi on 50%");
+                discount = 0.5;
+
+
             else
             {
                 Console.WriteLine("Oletko opiskelija k/e: ");
                 userInput = Console.ReadLine();
-
                 if (userInput == "k")
+                {
                     Console.WriteLine("Oletko mtk:n jäsen k/e: ");
-                userInput = Console.ReadLine();
+                    userInput = Console.ReadLine();
+                    if (userInput == "k")
+                        discount = 0.45;
+                    else
+                        discount = 0.6;
+                }
+                else
+                    Console.WriteLine("Syötä ikä");
+                    
             }
+            Console.WriteLine($"Lippusi hinta on {originalPrice * discount:C}");
         }
     }
+    
 }
