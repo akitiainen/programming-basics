f279fc4ca6b78ec7ecc2cbc3930b7e3b8d343ec1
akitiainen <aki.tiainen@hotmail.fi>
2018-09-29 22:02:02 +0300

    IfTask LipunHinta/Program.cs v1.0

--- IfTask LipunHinta/IfTask LipunHinta/Program.cs
+++ IfTask LipunHinta/IfTask LipunHinta/Program.cs
@@ -9,6 +9,7 @@ namespace IfTask_LipunHinta
             Console.WriteLine("Tämä ohjelma laskee maatalousnäyttelyn lipun hinnan");
             int originalPrice = 16;
             double discount = 0;
+            int age = 0;
             Console.WriteLine("Oletko varusmies k/e: ");
             string userInput = Console.ReadLine();
 
@@ -25,15 +26,38 @@ namespace IfTask_LipunHinta
                     Console.WriteLine("Oletko mtk:n jäsen k/e: ");
                     userInput = Console.ReadLine();
                     if (userInput == "k")
-                        discount = 0.45;
-                    else
                         discount = 0.6;
+                    else
+                        discount = 0.45;
                 }
                 else
-                    Console.WriteLine("Syötä ikä");
+                {
                     
+                    Console.WriteLine("Syötä ikä: ");
+                    string userInput1 = Console.ReadLine();
+                    bool isNumber = int.TryParse(userInput1, out age);
+                    if (isNumber)
+                    {
+                        if (age < 7)
+                        {
+                            discount = 1;
+                        }
+                        else if ((age < 7 || age > 15) && age < 65)
+                        {
+                            Console.WriteLine("Oletko mtk:n jäsen k/e: ");
+                            userInput = Console.ReadLine();
+                            if (userInput == "k")
+                                discount = 0.15;
+                        }
+                        else
+                        {
+                            discount = 0.5;
+                        }
+                    }
+                    else Console.WriteLine("Väärä syöte!");
+                }
             }
-            Console.WriteLine($"Lippusi hinta on {originalPrice * discount:C}");
+            Console.WriteLine($"Lippusi hinta on {originalPrice - originalPrice * discount:C}");
         }
     }
     
