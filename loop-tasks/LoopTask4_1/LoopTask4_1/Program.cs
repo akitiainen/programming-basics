2d22517c00151efad25fdc4c925484cdae194228
akitiainen <aki.tiainen@hotmail.fi>
2018-10-02 13:42:58 +0300

    LoopTask4_1 v1.0

---
+++ loop-tasks/LoopTask4_1/LoopTask4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace loopTask4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rndNumber;
+            rndNumber = rnd.Next();
+
+            Console.WriteLine("Ohjelma tulostaa 10 arvottua kokonaisluka ja niiden neliöjuuret.");
+            Console.WriteLine("#\tRND\tSQRT");
+            for (int i = 0; i < 10; i++)
+            {
+                rndNumber = rnd.Next(1, 100);
+                Console.WriteLine($"{i + 1}.\t{rndNumber}\t{Math.Sqrt(rndNumber)}");
+            }
+        }
+    }
+}
