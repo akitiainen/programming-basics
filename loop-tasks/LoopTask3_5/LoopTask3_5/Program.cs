053f37b64eae5adca6be16d030cb6b99fd419fd6
akitiainen <aki.tiainen@hotmail.fi>
2018-10-04 12:30:41 +0300

    LoopTask3_5 edited

--- loop-tasks/LoopTask3_5/LoopTask3_5/Program.cs
+++ loop-tasks/LoopTask3_5/LoopTask3_5/Program.cs
@@ -6,44 +6,44 @@ namespace LoopTask3_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This program adds the sums of even and odd numbers");
             int number = 0;
-            int i = 0;
+            int i = 1;
             int evenSum = 0;
             int oddSum = 0;
 
-            do
+            Console.WriteLine("This program adds the sums of even and odd numbers");
+            Console.Write("Input a number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number > 0)
             {
-                Console.Write("Input a number: ");
-                number = int.Parse(Console.ReadLine());
-                if (number == 0)
-                    Console.WriteLine("Väärä syöte!");
-            }
-            while (number == 0);
-
-            if(number>0)
-            {
-                if (i % 2 == 0)
+                while (i <= number)
                 {
-                    evenSum = evenSum + i; //evenSum += i;
+                    if (i % 2 == 0)
+                    {
+                        evenSum = evenSum + i; //evenSum += i;
+                    }
+                    else
+                    {
+                        oddSum = oddSum + i;
+                    }
+                    i++;
+                    break;
                 }
-                else
-                {
-                    oddSum = oddSum + i;
-                }
-                i++;
             }
             else
             {
-                if (i % 2 == 0)
+                while (i >= number)
                 {
-                    evenSum = evenSum - i; //evenSum += i;
+                    if (i % 2 == 0)
+                    {
+                        evenSum = evenSum - i;
+                    }
+                    else
+                    {
+                        oddSum = oddSum - i;
+                    }
+                    i++;
                 }
-                else
-                {
-                    oddSum = oddSum - i;
-                }
-                i--;
             }
             Console.WriteLine($"The sum of even numbers is {evenSum}");
             Console.WriteLine($"The sum of odd numbers is {oddSum}");
