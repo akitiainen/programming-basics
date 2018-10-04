95731949f1fb42d0ad86cc78ae4ddfa802d3fb11
akitiainen <aki.tiainen@hotmail.fi>
2018-10-04 09:41:01 +0300

    Loop Task 1 v1.0

--- loop-tasks/LoopTask1/LoopTask1/Program.cs
+++ loop-tasks/LoopTask1/LoopTask1/Program.cs
@@ -8,6 +8,33 @@ namespace LoopTask1
         {
             Console.WriteLine("This program solves factorials");
             int number = 0;
+            int i = 1;
+            int fact = 1;
+
+            //checks that the number is positive number
+            do
+            {
+                Console.Write("Input a number: ");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("Input error!");
+                }
+            }
+            while (number <= 0);
+            //calculate factorial 1 * 2 * 3 * ... * n
+            //while (i<= number)
+            //{
+            //    fact = fact * i;
+            //    i = i + 1;
+            //}
+            for (i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine($"{number}! = {fact}");
         }
     }
 }
