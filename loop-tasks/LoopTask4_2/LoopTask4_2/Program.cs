cc75bb5f0627a9c14e2c0b2374a8d539d07ee689
akitiainen <aki.tiainen@hotmail.fi>
2018-10-08 14:57:35 +0300

    LoopTask4_2 v1.0

--- loop-tasks/LoopTask4_2/LoopTask4_2/Program.cs
+++ loop-tasks/LoopTask4_2/LoopTask4_2/Program.cs
@@ -6,7 +6,24 @@ namespace LoopTask4_2
     {
         static void Main(string[] args)
         {
-         
+            int i;
+            int number;
+            Console.WriteLine("Tämä ohjelma tulostaa kertotaulun luvuille 1-9");
+            Console.WriteLine("\n");
+            Console.Write("Syötä luku: ");
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Väärä syöte!");
+                Console.WriteLine("Syötä luku: ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"Luvun {number} kertotaulu on");
+            Console.WriteLine("\n");
+            for (i = 1; i <= 9; i++)
+            {
+                Console.Write("{0} * {1} = {2} \n", number, i, number * i);
+            }
+            
         }
     }
 }
