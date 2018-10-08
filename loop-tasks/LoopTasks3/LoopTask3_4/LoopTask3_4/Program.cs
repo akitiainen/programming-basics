720eeb8a43d054c2b515ee6d544d8a20a0b0b5c2
akitiainen <aki.tiainen@hotmail.fi>
2018-10-08 16:07:42 +0300

    Moved LoopTasks3 to new directory

---
+++ loop-tasks/LoopTasks3/LoopTask3_4/LoopTask3_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LoopTask3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma laskee positiivisten ja negatiivisten lukujen summat");
+            int number = 0;
+            int i = 0;
+            int negSum = 0;
+            int posSum = 0;
+
+            do
+            {
+                Console.Write("Syötä luku: ");
+                number = int.Parse(Console.ReadLine());
+                if(number==0)
+                {
+                    Console.WriteLine("Väärä syöte!");
+                }
+            }
+            while (number==0);
+            {
+
+                if (number > 0)
+                {
+                    for (i = 0; i <= number; i++)
+                    {
+                        posSum += i;
+                    }
+                    Console.WriteLine($"Positiivisten lukujen summa: {posSum}");
+                }
+                else
+                {
+                    for (i = 0; i >= number; i--)
+                    {
+                        negSum += i;
+                    }
+                    Console.WriteLine($"Negatiivisten lukujen summa: {negSum}");
+                }
+            }
+        }
+    }
+}
