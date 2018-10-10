33bec3b48fc709c5f0e851e468e774df55d921b6
akitiainen <aki.tiainen@hotmail.fi>
2018-10-10 09:19:06 +0300

    LoopTask4_4 initialized

---
+++ loop-tasks/LoopTasks4/LoopTask4_4/LoopTask4_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTask4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä ohjelma simuloi kolikon heittoa");
+        }
+    }
+}
