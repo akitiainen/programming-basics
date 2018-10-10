6070ce3983a5799fbad15b17e283d4a8100ef539
akitiainen <aki.tiainen@hotmail.fi>
2018-10-10 09:56:43 +0300

    LoopTask4_4 v1.0

--- loop-tasks/LoopTasks4/LoopTask4_4/LoopTask4_4/Program.cs
+++ loop-tasks/LoopTasks4/LoopTask4_4/LoopTask4_4/Program.cs
@@ -6,7 +6,28 @@ namespace LoopTask4_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tämä ohjelma simuloi kolikon heittoa");
+            Random rnd = new Random();
+            int rndNumber;
+            int n;
+            int kr = 0;
+            int kl = 0;
+            Console.WriteLine("Tämä ohjelma simuloi kolikon heittoa\n");
+            Console.Write("Syötä numero montako kertaa haluat kolikkoa heitettävän: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                rndNumber = rnd.Next(2);
+                if (rndNumber == 1)
+                {
+                    kr = kr + 1;
+                }
+                else
+                {
+                    kl = kl + 1;
+                }
+            }
+            Console.WriteLine($"Kruuna heitettiin {kr} kertaa");
+            Console.WriteLine($"Klaava heitettiin {kl} kertaa");
         }
     }
 }
