77a9a93a207f913e30f51d88868bf63433104976
akitiainen <aki.tiainen@hotmail.fi>
2018-10-10 09:17:30 +0300

    LoopTask4_3 v1.0

--- loop-tasks/LoopTasks4/LoopTask4_3/LoopTask4_3/Program.cs
+++ loop-tasks/LoopTasks4/LoopTask4_3/LoopTask4_3/Program.cs
@@ -6,7 +6,21 @@ namespace LoopTask4_3
     {
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random();
+            int rndNumber;
+            rndNumber = rnd.Next();
+
+            for (int j = 0; j < 4; j++)
+            {
+                
+                for (int i = 0; i < 5; i++)
+                {
+                    rndNumber = rnd.Next(50);
+                    Console.Write($"{rndNumber} ");
+                    
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
