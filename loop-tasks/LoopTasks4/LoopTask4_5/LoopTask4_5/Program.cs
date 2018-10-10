1f3d85527cb7846263c8e1f27fe975f370f32226
akitiainen <aki.tiainen@hotmail.fi>
2018-10-10 19:57:59 +0300

    LoopTask4_5 initialized

---
+++ loop-tasks/LoopTasks4/LoopTask4_5/LoopTask4_5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoopTask4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vakioveikkauksen rivisi on:\n");
+            Random rnd = new Random();
+            for (int i = 1; i <= 13; i++)
+            {
+                rnd.NextDouble();
+                Console.WriteLine($"{rnd}");
+                {
+
+                }
+            }
+            
+            
+        }
+    }
+}
