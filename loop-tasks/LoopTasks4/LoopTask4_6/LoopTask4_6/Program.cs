bf166b2d2a12f2d5d1ab720ad694d90f0ebd57f7
akitiainen <aki.tiainen@hotmail.fi>
2018-10-11 09:55:34 +0300

    LoopTask4_6 v1.0

--- loop-tasks/LoopTasks4/LoopTask4_6/LoopTask4_6/Program.cs
+++ loop-tasks/LoopTasks4/LoopTask4_6/LoopTask4_6/Program.cs
@@ -6,7 +6,19 @@ namespace LoopTask4_6
     {
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random();
+            int rndNumber;
+            int counter = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                rndNumber = rnd.Next(1, 7);
+                if(rndNumber == 6)
+                {
+                    counter = counter + 1;
+                }Console.Write($"{rndNumber} ");
+                
+            }
+            Console.WriteLine($"Kuutonen heitettiin {counter} kertaa.");
         }
     }
 }
