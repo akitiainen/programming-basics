3bd74af100a472cf5be2ea825cb00f48cc4e6ec8
akitiainen <aki.tiainen@hotmail.fi>
2018-10-11 08:34:40 +0300

    LoopTask4_5 v1.0

--- loop-tasks/LoopTasks4/LoopTask4_5/LoopTask4_5/Program.cs
+++ loop-tasks/LoopTasks4/LoopTask4_5/LoopTask4_5/Program.cs
@@ -7,17 +7,25 @@ namespace LoopTask4_5
         static void Main(string[] args)
         {
             Console.WriteLine("Vakioveikkauksen rivisi on:\n");
+            double odds;
+
             Random rnd = new Random();
             for (int i = 1; i <= 13; i++)
             {
-                rnd.NextDouble();
-                Console.WriteLine($"{rnd}");
+                odds = rnd.NextDouble();
+                if(odds <= 0.4)
                 {
-
+                    Console.WriteLine($"{i}.\t 1");
+                }
+                else if(odds <= 0.6)
+                {
+                    Console.WriteLine($"{i}.\t X");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}.\t 2");
                 }
             }
-            
-            
         }
     }
 }
