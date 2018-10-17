3fba09c5f0b2f62216572bf2a6574d5f29c1e308
akitiainen <aki.tiainen@hotmail.fi>
2018-10-17 13:49:37 +0300

    LoopTask1 v1.0

--- function-tasks/functionTask1/functionTask1/Program.cs
+++ function-tasks/functionTask1/functionTask1/Program.cs
@@ -6,7 +6,35 @@ namespace functionTask1
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Functions - Task 1.\n");
+
+            int n;
+            while (true)
+            {
+                Console.Write("Tähtien lukumäärä: ");
+                n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                {
+                    Console.WriteLine($"Luku {n} ei ole sallittu luku");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string asterisks = getNAsterisks(n);
+            Console.WriteLine(asterisks);
+            Console.ReadKey();
+        }
+
+        static string getNAsterisks(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += '*';
+            }
+            return res;
         }
     }
 }
