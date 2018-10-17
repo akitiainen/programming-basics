238d3cee606c63bd7f455320d5d805699e1d6d29
akitiainen <aki.tiainen@hotmail.fi>
2018-10-17 14:51:20 +0300

    FunctionTask2 v1.0

--- function-tasks/FunctionTask2/FunctionTask2/Program.cs
+++ function-tasks/FunctionTask2/FunctionTask2/Program.cs
@@ -6,7 +6,27 @@ namespace FunctionTask2
     {
         static void Main(string[] args)
         {
-            
+            int x, y;
+            Intro(out x, out y);
+            Console.WriteLine(SmallerNumber(x,y));
+        }
+
+        static void Intro(out int num01, out int num02)
+        {
+            Console.Write("Input 1: ");
+            num01 = int.Parse(Console.ReadLine());
+            Console.Write("Input 2: ");
+            num02 = int.Parse(Console.ReadLine());
+        }
+
+        static string SmallerNumber(int a, int b)
+        {
+            if (a < b)
+                return $"Number {a} is smaller than number {b}";
+            else if (a > b)
+                return $"Number {b} is smaller than number {a}";
+            else
+                return "Numbers have the same value";
         }
     }
 }
