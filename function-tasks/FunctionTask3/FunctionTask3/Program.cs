8b29697fd27eed6ad38904f838ee0620e6595e24
akitiainen <aki.tiainen@hotmail.fi>
2018-10-20 12:43:03 +0300

    FunctionTask3
    Added TryParse loop to intro

--- function-tasks/FunctionTask3/FunctionTask3/Program.cs
+++ function-tasks/FunctionTask3/FunctionTask3/Program.cs
@@ -8,14 +8,17 @@ namespace FunctionTask3
         {
             int x;
             Intro(out x);
-            
         }
 
         static void Intro(out int num)
         {
-            Console.Write("Input a number: ");
-            num = int.Parse(Console.ReadLine());
+            Console.Write("Input a number between 0-50: ");
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.Clear();
+                Console.WriteLine("Input error");
+                Console.Write("Input a number: ");
+            }
         }
-
     }
 }
