fcc136af0d4c7292fb2e05bf773afa789233ca63
akitiainen <aki.tiainen@hotmail.fi>
2018-10-28 22:05:06 +0200

    RepetitionTask3 v1.1
    Clearing up the UI

--- Kertaustehtävät/RepetitionTask3/RepetitionTask3/Program.cs
+++ Kertaustehtävät/RepetitionTask3/RepetitionTask3/Program.cs
@@ -9,20 +9,20 @@ namespace RepetitionTask3
             int sum = 0;
             int input;
             Console.WriteLine("This program asks user for numbers and adds them togethger. To end the program enter '-1'.");
+            Console.WriteLine("Enter your numbers: ");
             do
             {
-                Console.Write("Enter a number: ");
+                
                 while (!int.TryParse(Console.ReadLine(), out input))
                 {
                     Console.WriteLine("Not a number!");
-                    Console.Write("Enter a number: ");
                 }
 
                 sum = sum + input;
             }
             while (input != -1);
 
-            Console.WriteLine(sum);
+            Console.WriteLine($"The sum of your numbers is {sum}!");
         }
     }
 }
