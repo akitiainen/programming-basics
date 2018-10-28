229606e4f68fffaa6d3573f087bdaa69a5d2252c
akitiainen <aki.tiainen@hotmail.fi>
2018-10-28 21:13:56 +0200

    RepetitionTask1 v1.0

---
+++ Kertaustehtävät/RepetitionTask1/RepetitionTask1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RepetitionTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program repeats whatever you write five times.");
+            Console.Write("Write something: ");
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
