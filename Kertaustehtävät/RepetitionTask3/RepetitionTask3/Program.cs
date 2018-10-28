6e8560296b7d1fb572057c67bb12756ea3458967
akitiainen <aki.tiainen@hotmail.fi>
2018-10-28 21:58:21 +0200

    RepetitionTask3 v1.0

---
+++ Kertaustehtävät/RepetitionTask3/RepetitionTask3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RepetitionTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int input;
+            Console.WriteLine("This program asks user for numbers and adds them togethger. To end the program enter '-1'.");
+            do
+            {
+                Console.Write("Enter a number: ");
+                while (!int.TryParse(Console.ReadLine(), out input))
+                {
+                    Console.WriteLine("Not a number!");
+                    Console.Write("Enter a number: ");
+                }
+
+                sum = sum + input;
+            }
+            while (input != -1);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
