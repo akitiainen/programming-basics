52f6b05caff0122e72c2870b4f3d3cbb32d1c23c
akitiainen <aki.tiainen@hotmail.fi>
2018-10-28 21:37:47 +0200

    RepetitionTask2 v1.0

---
+++ Kertaustehtävät/RepetitionTask2/RepetitionTask2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace RepetitionTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program repeats whatever you write as many times as your writing has letters.");
+            Console.Write("Write something: ");
+            string input = Console.ReadLine();
+            int numberOfLetters = input.Count(char.IsLetterOrDigit);
+            numberOfLetters = numberOfLetters + input.Count(char.IsPunctuation);
+
+            for (int i = 1; i <= numberOfLetters; i++)
+            {
+                Console.WriteLine($"{i}. {input}");
+            }
+        }
+    }
+}
