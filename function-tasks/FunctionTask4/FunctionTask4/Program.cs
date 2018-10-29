a5e2780d98c5b77da94a56b638287e2925788a23
akitiainen <aki.tiainen@hotmail.fi>
2018-10-29 11:49:06 +0200

    FunctionTask4 updated

--- function-tasks/FunctionTask4/FunctionTask4/Program.cs
+++ function-tasks/FunctionTask4/FunctionTask4/Program.cs
@@ -6,7 +6,31 @@ namespace FunctionTask4
     {
         static void Main(string[] args)
         {
-            
+            int indx = 0;
+            Console.WriteLine("Syötä 10 lukua");
+            Console.WriteLine($"Suurin luku oli {LargestNumber(ref indx)} oli {indx}.");
+        }
+
+        static int LargestNumber(ref int indx)
+        {
+            int largestNumber = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i+1}.");
+                int input = int.Parse(Console.ReadLine());
+                if (input < 0)
+                {
+                    Console.WriteLine("väärä syöte, syötä positiivinen luku");
+                    i--;
+                }
+
+                if (input > largestNumber)
+                {
+                    largestNumber = input;
+                    indx = i + 1;
+                }
+            }
+            return largestNumber;
         }
     }
 }
