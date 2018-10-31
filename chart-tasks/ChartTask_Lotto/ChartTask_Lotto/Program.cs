c3650968695cba43191316e7c85967034fbb3275
akitiainen <aki.tiainen@hotmail.fi>
2018-10-31 14:50:31 +0200

    ChartTask_Lotto initialized

---
+++ chart-tasks/ChartTask_Lotto/ChartTask_Lotto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChartTask_Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
