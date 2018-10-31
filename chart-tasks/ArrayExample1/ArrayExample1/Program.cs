233bb3429a2fbacbdc97273ab2cc82a88b8ee138
akitiainen <aki.tiainen@hotmail.fi>
2018-10-31 13:04:40 +0200

    ArrayExample v1.0

--- chart-tasks/ArrayExample1/ArrayExample1/Program.cs
+++ chart-tasks/ArrayExample1/ArrayExample1/Program.cs
@@ -6,7 +6,24 @@ namespace ArrayExample1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma alustaa kokonaisluku taulukon (10 alkiota), johon arvotaan lukuja 0 ... 100");
+            int[] numbers = new int[1000];
+            Random rnd = new Random();
+
+            //set values to array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+
+            //Get values from array numbers
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ohjelman suoritus on päättynyt");
         }
     }
 }
