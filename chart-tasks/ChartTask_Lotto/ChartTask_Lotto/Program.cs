507a6af29fbc1fd746c84887c675002b5b636405
akitiainen <aki.tiainen@hotmail.fi>
2018-10-31 15:50:48 +0200

    ChartTask_Lotto v1.0

--- chart-tasks/ChartTask_Lotto/ChartTask_Lotto/Program.cs
+++ chart-tasks/ChartTask_Lotto/ChartTask_Lotto/Program.cs
@@ -6,7 +6,45 @@ namespace ChartTask_Lotto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma arpoo lauantain lottonumerot ja tulostaa arvotut luvut.");
+            int[] lottoNumbers = new int[40];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                int rndNumber = rnd.Next(40);
+                if (lottoNumbers[rndNumber] == 0)
+                    lottoNumbers[rndNumber] = 1;
+                else
+                    i--;
+            }
+
+            while (true)
+            {
+                int rndNumber = rnd.Next(40);
+                if (lottoNumbers[rndNumber] == 0)
+                {
+                    lottoNumbers[rndNumber] = 2;
+                    break;
+                }
+            }
+            int plusNumber = rnd.Next(40)+1;
+
+            Console.Write("Arvottiin oikeat numerot: ");
+            for (int i = 0; i < lottoNumbers.Length; i++)
+            {
+                if(lottoNumbers[i] == 1)
+                    Console.Write($"{i+1}\t");
+               
+            }
+
+            for (int i = 0; i < lottoNumbers.Length; i++)
+            {
+                if (lottoNumbers[i] == 2)
+                    Console.WriteLine($"\nLisänumero: {i + 1}");
+
+            }
+            Console.WriteLine($"Plussanumero: {plusNumber}");
         }
     }
 }
