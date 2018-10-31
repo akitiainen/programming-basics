7434b4b4e622c3372d1dd4ab6fdb1f503b62f44d
akitiainen <aki.tiainen@hotmail.fi>
2018-10-31 13:33:15 +0200

    ChartTask2 initialized

---
+++ chart-tasks/ChartTask2/ChartTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChartTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
