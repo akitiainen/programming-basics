4a043467b07743576083c51b24d99f90df09794b
akitiainen <aki.tiainen@hotmail.fi>
2018-10-31 13:31:23 +0200

    ChartTask1 v1.0

--- chart-tasks/ChartTask1/ChartTask1/Program.cs
+++ chart-tasks/ChartTask1/ChartTask1/Program.cs
@@ -6,7 +6,19 @@ namespace ChartTask1
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Hinnasto");
+            decimal[] productPrice = new decimal[] {7.96m, 72.00m, 99.90m};
+            int[] productAmount = new int[] {1, 3, 2};
+            decimal[] productTotalPrice = new decimal[3];
+
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                productTotalPrice[i] = productPrice[i] * productAmount[i];
+                Console.WriteLine($"Tuote {i + 1}: {productTotalPrice[i]:c}");
+            }
+
+
         }
     }
 }
