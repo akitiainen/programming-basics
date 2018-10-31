eac402d13d6f0f81cb9417b7b7e90ff6bdc21567
akitiainen <aki.tiainen@hotmail.fi>
2018-10-31 14:30:26 +0200

    ChartTask2 v1.0

--- chart-tasks/ChartTask2/ChartTask2/Program.cs
+++ chart-tasks/ChartTask2/ChartTask2/Program.cs
@@ -6,7 +6,28 @@ namespace ChartTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Ohjelma arpoo 100 lukua 0 ... 50 väiltä ja ilmoittaa luvuista saadun keskiarvon.");
+            decimal[] iT = new decimal[100];
+            Random rnd = new Random();
+
+            //lukujen arvonta ja taulukon tulostus
+            for (int i = 0; i < iT.Length; i++)
+            {
+                iT[i] = rnd.Next(51);
+                Console.WriteLine($"{i + 1}.\t{iT[i]}");
+            }
+
+            decimal sum = 0;
+            //laske summa ja keskiarvo
+            for (int i = 0; i < iT.Length; i++)
+            {
+                sum = sum + iT[i];
+            }
+            decimal avrg = sum / 100;
+
+            //tulosta keskiarvo
+            Console.WriteLine($"Ka. = {avrg}");
         }
     }
 }
