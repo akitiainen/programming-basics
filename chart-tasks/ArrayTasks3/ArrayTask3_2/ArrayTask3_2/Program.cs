201dc9fc33917cb20119034bc82176a213309e2f
akitiainen <aki.tiainen@hotmail.fi>
2018-11-10 19:19:06 +0200

    ArrayTask3_2 v1.0

--- chart-tasks/ArrayTasks3/ArrayTask3_2/ArrayTask3_2/Program.cs
+++ chart-tasks/ArrayTasks3/ArrayTask3_2/ArrayTask3_2/Program.cs
@@ -6,6 +6,22 @@ namespace ArrayTask3_2
     {
         static void Main(string[] args)
         {
+            int[,] raffle = new int[10, 20];
+            RndRaffle(ref raffle);
+        }
+
+        static void RndRaffle(ref int[,] arrRaffle)
+        {
+            Random rnd = new Random();
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    arrRaffle[x, y] = rnd.Next(101);
+                    Console.WriteLine($"{x} {y}\t{arrRaffle[x, y]:d2} ");
+                }
+            }
             
         }
     }
