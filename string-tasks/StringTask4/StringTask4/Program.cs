6198ea50ee31096fc96a495b919af574b9cc4d68
akitiainen <aki.tiainen@hotmail.fi>
2018-11-12 11:13:59 +0200

    StringTask4 initialized

---
+++ string-tasks/StringTask4/StringTask4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write something ");
+            string input = Console.ReadLine();
+            string tupni = input;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string x = input[i].ToString();
+                tupni.Insert(input.Length-1-i, x);
+            }
+            Console.WriteLine(tupni);
+        }
+    }    
+}
