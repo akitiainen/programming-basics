10fc77dd2208ff2440f651463817da11ee066238
akitiainen <aki.tiainen@hotmail.fi>
2018-11-12 09:48:37 +0200

    StringTask3 v1.0

--- string-tasks/StringTask3/StringTask3/Program.cs
+++ string-tasks/StringTask3/StringTask3/Program.cs
@@ -9,14 +9,17 @@ namespace StringTask3
             Console.WriteLine("Write something: ");
             string S = Console.ReadLine();
             S = S.ToUpper();
+            int letterCounter = 0;
             for (int i = 0; i < S.Length; i++)
             {
                 S.IndexOf(S[i]);
-                if(S[i] = "L")
+                if (S[i].ToString() == "L")
                 {
                     Console.WriteLine(S[i]);
+                    letterCounter++;
                 }
             }
+            Console.WriteLine($"Your input '{S}' has {letterCounter} letter L's");
         }
     }
 }
