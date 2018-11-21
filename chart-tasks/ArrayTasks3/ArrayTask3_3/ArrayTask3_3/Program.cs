fdd4a791550279a39679dc5fdfdabc23dd9265f2
akitiainen <aki.tiainen@hotmail.fi>
2018-11-21 11:05:06 +0200

    ArrayTask3_3 initialized

---
+++ chart-tasks/ArrayTasks3/ArrayTask3_3/ArrayTask3_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrT_1 = RndArray(10);
+            int[] arrT_2 = RndArray(10);
+        }
+
+        static int [] RndArray(int length)
+        {
+            Random rnd = new Random();
+            int[] arrT = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                arrT[i] = rnd.Next(20);
+            }
+            return arrT;
+        }
+    }
+}
