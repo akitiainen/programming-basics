3c5249fabc40548401fc90bbacf67d2f9a77fc9b
akitiainen <aki.tiainen@hotmail.fi>
2018-11-22 08:48:57 +0200

    SelfEvaluationTask1 initialized

---
+++ SelfEvaluationTask1/SelfEvaluationTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SelfEvaluationTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tulostaa tähtiä");
+            int [] numbers = new int[] {2,5,10,7,3};
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                PrintStars(numbers[i]);
+            }
+            
+        }
+
+        static void PrintStars(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
