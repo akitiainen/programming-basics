a7d568eda4293e2c16525e4107be072436d268a8
akitiainen <aki.tiainen@hotmail.fi>
2018-11-23 20:35:56 +0200

    VowelCounter v1.0

--- merkkijonot/VowelCounter/VowelCounter/Program.cs
+++ merkkijonot/VowelCounter/VowelCounter/Program.cs
@@ -6,7 +6,38 @@ namespace VowelCounter
     {
         static void Main(string[] args)
         {
-            
+            string input = Intro();
+            int counter = VowelCounter(input);
+            PrintData(input, counter);
+        }
+
+        static string Intro()
+        {
+            Console.Write("kirjoita jotakin: ");
+            return Console.ReadLine();
+        }
+
+        static int VowelCounter(string userInput)
+        {
+            string vowels = "AEIOUYÅÄÖ";
+            string inputTmp = userInput.ToUpper();
+            int c = 0;
+            for (int i = 0; i < vowels.Length; i++)
+            {
+                for (int j = 0; j < userInput.Length; j++)
+                {
+                    if (inputTmp[j] == vowels[i])
+                    {
+                        c++;
+                    }
+                }
+            }
+            return c;
+        }
+
+        static void PrintData(string word, int number)
+        {
+            Console.WriteLine($"Sanassa {word} on {number} vokaalia!");
         }
     }
 }
