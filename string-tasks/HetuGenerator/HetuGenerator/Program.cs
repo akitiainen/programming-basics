cce66aac15ab78d2c58526711380a13a618adc01
akitiainen <aki.tiainen@hotmail.fi>
2018-11-29 18:22:23 +0200

    HetuGenerator updated

--- string-tasks/HetuGenerator/HetuGenerator/Program.cs
+++ string-tasks/HetuGenerator/HetuGenerator/Program.cs
@@ -6,7 +6,67 @@ namespace HetuGenerator
     {
         static void Main(string[] args)
         {
-            
+            do
+            {
+                string date = Intro();
+                date = Cleaner(date);
+                if (DateChecker(date))
+                {
+                    string end = EndGenerator();
+                    Console.WriteLine(end);
+                    break;
+                }
+            }
+            while (true);
         }
+
+        static string Intro()
+        {
+            Console.Write("Syötä syntymäpäivä muodossa pp/kk/vvvv: ");
+            return Console.ReadLine();
+        }
+
+        static string Cleaner(string x)
+        {
+            x = x.Replace("/", "");
+            return x;
+        }
+
+        static bool DateChecker(string d)
+        {
+            int day = int.Parse(d.Substring(0, 2));
+            int month = int.Parse(d.Substring(2, 2));
+            int year = int.Parse(d.Substring(4, 4));
+
+            try
+            {
+                DateTime dd = new DateTime(year, month, day);
+            }
+            catch
+            {
+                Console.WriteLine("Päivämäärä on virheellinen");
+                return false;
+            }
+            return true;
+        }
+
+        static string EndGenerator()
+        {
+            Random end = new Random();
+
+            int[] endArray = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                if (endArray[i] == 0)
+                    endArray[i] = end.Next(9);
+                else if (endArray[i] == 1)
+                    endArray[i] = end.Next(10);
+                else
+                    endArray[i] = end.Next(2, 10);
+            }
+            return endArray.ToString();
+        }
+
+       
     }
 }
