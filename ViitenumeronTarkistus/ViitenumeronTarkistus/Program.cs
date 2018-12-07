2f32df996e18f9dbce311c9b8a8cce4f7126f039
akitiainen <aki.tiainen@hotmail.fi>
2018-12-07 11:58:33 +0200

    ViitenumeronTarkistus updated

--- ViitenumeronTarkistus/ViitenumeronTarkistus/Program.cs
+++ ViitenumeronTarkistus/ViitenumeronTarkistus/Program.cs
@@ -6,7 +6,24 @@ namespace ViitenumeronTarkistus
     {
         static void Main(string[] args)
         {
-            
+            Intro();
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Console.Clear();
+                    Funkkari1();
+                    break;
+            }
+        }
+
+        static void Intro()
+        {
+            Console.WriteLine("1. ohjelma tarkistaa kotimaisen viitenumeron\n2. luo kotimaisen viitenumeron\n3. luo halutun määrän viitenumeroita ja tallentaa ne tekstitiedostoon");
+        }
+
+        static void Funkkari1()
+        {
+            Console.WriteLine("lol");
         }
     }
 }
