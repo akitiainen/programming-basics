67a5716edc4c2083205a5cf18e1343d180e39b0e
akitiainen <aki.tiainen@hotmail.fi>
2018-12-20 16:22:09 +0200

    ArrayAndFunctionTask2 done

---
+++ Kertaustehtävät/ArrayAndFunctionTask2/ArrayAndFunctionTask2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ArrayAndFunctionTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string[] names = new string[] { "MacBook Air", "Jolla", "Nokia" };
+            decimal[] prices = new decimal[] { 928.90m, 349.00m, 99.80M };
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{names[i].PadRight(15)}{prices[i].ToString("C", CultureInfo.CurrentCulture)}");
+            }
+        }
+    }
+}
