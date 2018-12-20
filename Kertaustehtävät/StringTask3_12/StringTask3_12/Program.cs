7826d64dfe246b467321d5ecf3e964ba5e0f0d66
akitiainen <aki.tiainen@hotmail.fi>
2018-12-20 18:05:14 +0200

    StringTask3_12 initialized

---
+++ Kertaustehtävät/StringTask3_12/StringTask3_12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace StringTask3_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = Title();
+            
+        }
+
+        static string[] Title()
+        {
+            string s = "THE RISE OF THE PLANET OF THE APES";
+            string[] x = s.Split(' ');
+            return x;
+        }
+    }
+}
