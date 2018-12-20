63a0c10afdbfbe7c7df8530cb57569367441dfb0
akitiainen <aki.tiainen@hotmail.fi>
2018-12-20 15:11:49 +0200

    Kertaustehtävä12_11 done

---
+++ Kertaustehtävät/Kertaustehtävät12_11/Kertaustehtävät12_11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kertaustehtävät12_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num01;
+            int num02 = 0;
+            int sum = 0;
+            Console.WriteLine("Ohjelma kysyy lukuja kunnes syötät kaksi samaa lukua peräkkäin.");
+            Console.WriteLine("Syötä luku:");
+            while (true)
+            {
+                
+                while (!int.TryParse(Console.ReadLine(), out num01))
+                {
+                    Console.WriteLine("Virhe! Syötä luku!");
+                }
+                sum = sum + num01;
+                if (num01 == num02)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+                else
+                    num02 = num01;
+            }
+        }
+    }
+}
