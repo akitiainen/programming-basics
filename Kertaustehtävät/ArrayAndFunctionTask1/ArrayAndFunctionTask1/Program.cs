b3c2ed26e93498dc40887a688760fe36a3ed9bb8
akitiainen <aki.tiainen@hotmail.fi>
2018-12-20 15:54:02 +0200

    ArrayAnfFunctionTask1 done

---
+++ Kertaustehtävät/ArrayAndFunctionTask1/ArrayAndFunctionTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayAndFunctionTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string asterisks = getNAsterisks(numbers[i]);
+                Console.WriteLine(asterisks);
+            }
+        }
+
+        static string getNAsterisks(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += '*';
+            }
+            return res;
+        }
+    }
+}
