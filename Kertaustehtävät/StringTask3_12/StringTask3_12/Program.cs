d94f08581d42e5675185ea9fc62605a496665e47
akitiainen <aki.tiainen@hotmail.fi>
2018-12-23 18:28:28 +0200

    StringTask3_12 updated

--- Kertaustehtävät/StringTask3_12/StringTask3_12/Program.cs
+++ Kertaustehtävät/StringTask3_12/StringTask3_12/Program.cs
@@ -8,13 +8,32 @@ namespace StringTask3_12
         static void Main(string[] args)
         {
             string[] list = Title();
-            
+            list = LOL(list);
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
         }
 
         static string[] Title()
         {
             string s = "THE RISE OF THE PLANET OF THE APES";
+            s = s.ToUpper();
             string[] x = s.Split(' ');
+            return x;
+        }
+
+        static string[] LOL(string[] x)
+        {
+            string s = string.Empty;
+            string[] a = new string[] {"THE", "OF", "A", "TO", "AND", "BUT", "FOR", "AT", "BY", "FROM"};
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+
+                }
+            }
             return x;
         }
     }
